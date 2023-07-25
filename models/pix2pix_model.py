26418e98be037fc56e4af2ce803fc8ffd59a75d9
rvecchiarelli <rvecchiarell@student.ethz.ch>
2023-07-25 14:43:25 +0200

    Update pix2pix_model.py

--- models/pix2pix_model.py
+++ models/pix2pix_model.py
@@ -34,7 +34,7 @@ class Pix2PixModel(BaseModel):
         if is_train:
             parser.set_defaults(pool_size=0, gan_mode='vanilla')
             parser.add_argument('--lambda_L1', type=float, default=100.0, help='weight for L1 loss')
-            parser.add_argument('--lambda_Lmass', type=float, default=50.0, help='weight for L1 loss')
+            parser.add_argument('--lambda_Lmass', type=float, default=15.0, help='weight for L1 loss')
 
         return parser
 
